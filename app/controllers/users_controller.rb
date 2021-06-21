1d84163458cbc54c838848c21a718b450ac0dbf9
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-21 15:52:24 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
+
   def show
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
-    card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
-    
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+
     redirect_to new_card_path and return unless card.present?
 
-    customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+    customer = Payjp::Customer.retrieve(card.customer_token)
     @card = customer.cards.first
   end
-  
+
   def update
-    if current_user.update(user_params) # 更新出来たかを条件分岐する
-      redirect_to root_path # 更新できたらrootパスへ
+    if current_user.update(user_params)
+      redirect_to root_path
     else
-      redirect_to action: "show" # 失敗すれば再度マイページへ
+      redirect_to action: "show"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email) # 編集出来る情報を制限
+    params.require(:user).permit(:name, :email)
   end
+
 end
