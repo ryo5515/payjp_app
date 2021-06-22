7d0ec742d609d3cfd906af1a529e8e70617df3f1
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-22 15:35:18 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order  # 「find_item」を動かすアクションを限定
+
   def index
     @items = Item.all
   end
+
+  def order # 購入する時のアクションを定義
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id]) # 購入する商品を特定
+  end
+
 end
