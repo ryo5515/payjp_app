af7506d283b99dab1ad62bcea2ac86446e75b750
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-22 15:36:54 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,30 @@
 class ItemsController < ApplicationController
-  before_action :find_item, only: :order  # 「find_item」を動かすアクションを限定
+  before_action :find_item, only: :order
 
   def index
     @items = Item.all
   end
 
-  def order # 購入する時のアクションを定義
+  def order
+    redirect_to new_card_path and return unless current_user.card.present?
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer_token = current_user.card.customer_token
+    Payjp::Charge.create(
+      amount: @item.price,
+      customer: customer_token,
+      currency: 'jpy' 
+    )
+
+    ItemOrder.create(item_id: params[:id])
+
+    redirect_to root_path
   end
 
   private
 
   def find_item
-    @item = Item.find(params[:id]) # 購入する商品を特定
+    @item = Item.find(params[:id])
   end
 
 end
